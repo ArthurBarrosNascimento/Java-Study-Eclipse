ca9768f7aef3d7e31166144a8ae038a491a031a2
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-04 15:46:38 -0300

    Composition Exercise

---
+++ src/entities/Department.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Department {
+	private String name;
+	
+	public Department() {
+	}
+	
+	public Department(String value) {
+		// TODO Auto-generated constructor stub
+		this.name = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/entities/enums/WorkedLevel.java
@@ -0,0 +1,7 @@
+package entities.enums;
+
+public enum WorkedLevel {
+	JUNIOR,
+	MID_LEVEL,
+	SENIOR;
+}
